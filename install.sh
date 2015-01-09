87b922c9523bd50a6d58e4fbc77ff13a2936e2d5
Luke Askew <lukeaskew@gmail.com>
2015-01-09 09:51:39 -0500

    add zsh configuration

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ sh "./defaults.sh"
 sh "./binaries.sh"
 sh "./apps.sh"
 
-# install .zshrc
-
+# configure .zshrc
+curl -o .zshrc https://gist.githubusercontent.com/LukeAskew/2357260973f7d8f42143/raw/cae2df17ebabde171a9db01d02d0692129b91278/.zshrc
 
 killall Dock
 killall Finder
