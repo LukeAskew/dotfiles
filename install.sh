f758053833afb655818f62a2a7df3882e0f6534c
Luke Askew <lukeaskew@gmail.com>
2015-01-12 13:36:21 -0500

    base64 -> b64 alias

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sh "./apps.sh"
 
 # configure .zshrc
 echo "/usr/local/bin/zsh" >> /etc/shells
-curl -o .zshrc https://gist.githubusercontent.com/LukeAskew/2357260973f7d8f42143/raw/7d966992a610529c1daf73af0aa47d4c32b23d62/.zshrc
+curl -o .zshrc https://gist.githubusercontent.com/LukeAskew/2357260973f7d8f42143/raw/19228adbf206ae5156f1f38df6802b66bad5a316/.zshrc
 
 killall Dock
 killall Finder
